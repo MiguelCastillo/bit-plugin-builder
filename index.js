adcb6bf4d9d6e2936149ea389705a9d9816edf7a
Miguel Castillo <manchagnu@gmail.com>
2016-07-26 21:43:06 -0400

    Added better data merging for aggregating settings

--- index.js
+++ index.js
@@ -1,81 +1,68 @@
 var utils = require("belty");
 
-
 var defaults = {
   resolve: [],
   fetch: [],
   transform: [],
   dependency: [],
-  precompile: []
+  precompile: [],
+  extensions: []
 };
-
 
 function PluginBuilder(options) {
-  this._configuration = configure(utils.merge({}, defaults), options);
+  if (!(this instanceof PluginBuilder)) {
+    return new PluginBuilder(options);
+  }
+
+  this._configuration = merge(utils.merge({}, defaults), options);
 }
 
-
 PluginBuilder.prototype.configure = function(options) {
-  this._configuration = configure(this._configuration, options);
+  merge(this._configuration, options);
   return this;
 };
-
 
 PluginBuilder.prototype.build = function() {
   return utils.merge({}, this._configuration);
 };
 
-
 PluginBuilder.create = function(options) {
   return new PluginBuilder(options);
 };
 
-
-function configure(pluginConfig, options) {
+function merge(pluginConfig, options) {
   options = options || {};
 
-  Object.keys(options)
+  var updateKeys = Object
+    .keys(options)
     .filter(function(option) {
       return defaults.hasOwnProperty(option);
-    })
-    .map(function(option) {
-      var value = options[option];
-      return {
-        name: option,
-        value: utils.toArray(value)
-      };
-    })
-    .forEach(function(config) {
-      pluginConfig[config.name] = pluginConfig[config.name].concat(config.value);
     });
+
+  utils.extend(pluginConfig, mergeState(pluginConfig, utils.pick(options, updateKeys)));
 
   var matches = options.match || options.matches;
   if (matches) {
-    pluginConfig.matches = configureRule(matches);
+    pluginConfig.matches = mergeState(pluginConfig.matches, matches);
   }
 
   var ignores = options.ignore || options.ignores;
   if (ignores) {
-    pluginConfig.ignores = configureRule(ignores);
-  }
-
-  var extensions = options.extensions;
-  if (extensions) {
-    pluginConfig.extensions = utils.toArray(extensions);
+    pluginConfig.ignores = mergeState(pluginConfig.ignores, ignores);
   }
 
   return pluginConfig;
 };
 
+function mergeState(currentState, newState) {
+  currentState = currentState || {};
 
-function configureRule(rule) {
   return Object
-    .keys(rule)
+    .keys(newState)
     .reduce(function(result, key) {
-      result[key] = utils.toArray(rule[key]);
+      result[key] = (currentState[key] || []).concat(utils.toArray(newState[key]));
       return result;
     }, {});
 }
-
 
 module.exports = PluginBuilder;
